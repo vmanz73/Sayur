a7932c8ada3b860eaf72e2c364d3748c8d04337d
vmanz73 <firman.syahmaulana@yahoo.com>
2020-06-18 16:23:46 +0700

    add lcd

--- LCD/test.py
+++ LCD/test.py
@@ -1,10 +1,10 @@
-from LCD import lcddriver
+import lcddriver
 from time import *
  
 lcd = lcddriver.lcd()
 lcd.lcd_clear()
  
-lcd.lcd_display_string("321", 1)
+lcd.lcd_display_string("123", 1)
 lcd.lcd_display_string("      RaspberryPi.de", 2)
 lcd.lcd_display_string("", 3)
 lcd.lcd_display_string("HD44780 I2C Tutorial", 4)
