bb8673c4048493a12729bedd3000dcbb6b8d7cdd
vmanz73 <firman.syahmaulana@yahoo.com>
2020-06-20 15:38:07 +0700

    add cnn

--- test.py
+++ test.py
@@ -6,6 +6,17 @@ lcd.lcd_clear()
  
 import time
 import sys
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+from imutils.video import VideoStream
+from imutils.video import FPS
+import time
+import numpy as np
+import connection
+import get_hargadb as harga
+import imutils
+import cv2
+
 
 EMULATE_HX711=False
 
@@ -19,54 +30,79 @@ else:
 
 def cleanAndExit():
     print("Cleaning...")
-
+    
     if not EMULATE_HX711:
         GPIO.cleanup()
-        
+    break
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
     print("Bye!")
     sys.exit()
 
 hx = HX711(5, 6)
 
-# I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
-# Still need to figure out why does it change.
-# If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
-# There is some code below to debug and log the order of the bits and the bytes.
-# The first parameter is the order in which the bytes are used to build the "long" value.
-# The second paramter is the order of the bits inside each byte.
-# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
+
 hx.set_reading_format("MSB", "MSB")
 
-# HOW TO CALCULATE THE REFFERENCE UNIT
-# To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
-# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
-# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-#hx.set_reference_unit(113)
+
 hx.set_reference_unit(referenceUnit)
 
 hx.reset()
 
 hx.tare()
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+print("[INFO] loading network...")
+model = load_model("sayur.model")
+
+fps = FPS().start()
 
 print("Tare done! Add weight now...")
 
 harga = 6000
 while True:
     try:
+        frame = vs.read()
+
+        # pre-process the image for classification
+        image = cv2.resize(frame, (28, 28))
+        image = image.astype("float") / 255.0
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
 
         val = hx.get_weight(5)
         print(val)
+        (veg) = model.predict(image)[0].copy()
+        name = ["Kentang","Kubis","Mentimun","Terung","Tomat","Wortel"]
+        veg = veg.tolist()
+        probMax = veg.index(max(veg))
+        label = name[probMax]
+        proba = max(veg)
+
+        label = "{}: {:.2f}%".format(label, proba * 100)
+
+        #Get price
+        price = harga.get_harga(name[probMax])
+    
 
         lcd.lcd_display_string("Berat : "+str(int(val))+" g           ", 1)
-        lcd.lcd_display_string("Jenis Sayur : Tomat", 2)
-        lcd.lcd_display_string("Harga/kg : Rp. " + str(harga)+ "      ", 3)
+        lcd.lcd_display_string("Jenis Sayur : " + label, 2)
+        lcd.lcd_display_string("Harga/kg : Rp. " + str(price)+ "      ", 3)
         lcd.lcd_display_string("Total : Rp."+str(harga*int(harga)/1000) + "    ", 4)
 
 
         hx.power_down()
         hx.power_up()
         time.sleep(0.1)
+        fps.update()
 
+        # show the output image
+        cv2.imshow("Frame", frame)
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
