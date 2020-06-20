b08cc518b7e8d5263995a961a28a4f20b3f984f0
vmanz73 <firman.syahmaulana@yahoo.com>
2020-06-20 12:45:28 +0700

    add load cell to test

---
+++ test copy 2.py
@@ -0,0 +1,10 @@
+import lcddriver
+from time import *
+ 
+lcd = lcddriver.lcd()
+lcd.lcd_clear()
+ 
+lcd.lcd_display_string("Berat : 7 g", 1)
+lcd.lcd_display_string("Jenis Sayur : Tomat", 2)
+lcd.lcd_display_string("Harga/kg : Rp.6000 ", 3)
+lcd.lcd_display_string("Total : Rp.42000", 4)
